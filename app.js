2d47bd437d9c0517fd0f7395bea4086c3832a3bd
Rahul Bir <rahul@rahulbir.com>
2016-04-08 14:51:29 -0600

    Added a test endpoint

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.get('/', function (req, res){
     res.send('Hello world from Distelli & Docker!');
 });
 
+app.get('/test', function(req, res){
+	res.send('We are testing an endpoint to break a test.');
+});
+
 module.exports = app;
